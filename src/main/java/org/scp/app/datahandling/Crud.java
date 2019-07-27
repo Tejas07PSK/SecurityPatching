5e837156ab41ec22812d6938e631a6430983ba52
Palash Sarkar <palashsarkar0007@gmail.com>
2019-07-28 01:57:25 +0530

    Added Crud.java !!

---
+++ src/main/java/org/scp/app/datahandling/Crud.java
@@ -0,0 +1,21 @@
+/*  -> Designed for testing and development purposes.
+ *  -> Project to design a 'SecurityPatching' prototype.
+ *  -> Development Phase -- Intermediate.
+ *  -> Project Type -- Educational.
+ *  -> Owner/Designer of code file :
+ *             @ Name - Palash Sarkar.
+ *             @ Email - palashsarkar0007@gmail.com.
+ *  -> Copyright Norms - Every piece of code given below has been written by 'Palash Sarkar (Tj07)'©,
+ *                       and he holds the rights to the file. Not meant to be
+ *                       copied or tampered with, without prior permission from the author.
+ *  -> Guide - Balaji Chinthakalaya.
+ */
+
+package org.scp.app.datahandling;
+
+interface Crud {
+
+    public String insert ( String ... vals );
+    public String retrieve ( String ... vals );
+
+}
